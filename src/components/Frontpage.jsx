53439331b633ce6ddd106903d8f73002b87f99da
Saikrishna <potharaboinasaikrishna1@gmail.com>
2026-05-22 14:31:19 +0530

    Added story with image completed

--- src/components/Frontpage.jsx
+++ src/components/Frontpage.jsx
@@ -1,8 +1,8 @@
 import React, { useRef, useState } from "react";
 import "../App.css"
 
-const AddStory = () => {
-    const [image, setImage] = useState(null);
+export const AddStory = () => {
+    const [stories, setStories] = useState([]);
     const fileRef = useRef(null);
 
     const handleClick = () => {
@@ -13,7 +13,8 @@ const AddStory = () => {
         const file = e.target.files[0];
 
         if (file) {
-            setImage(URL.createObjectURL(file));
+            const imageUrl = URL.createObjectURL(file);
+            setStories((prev) => [...prev, imageUrl])
         }
     };
 
@@ -35,32 +36,22 @@ const AddStory = () => {
                     className="relative w-[78px] h-[78px] rounded-full bg-gradient-to-tr from-yellow-400 via-pink-500 to-purple-600 p-[2.5px]"
                 >
                     <div className="w-full h-full rounded-full bg-white p-[2px]">
-
-                        {image ? (
-                            <img
-                                src={image}
-                                alt="story"
-                                className="w-full h-full rounded-full object-cover"
-                            />
-                        ) : (
-                            <img
-                            // src="/instagram-stories.png"
-                            // alt="story"
-                            // className="w-full h-full rounded-full object-cover"
-                            />
-                        )}
-
                         <div className="absolute bottom-0 right-0 w-6 h-6 rounded-full bg-blue-500 border-2 border-white flex items-center justify-center text-white text-lg leading-none">
                             +
                         </div>
                     </div>
                 </button>
-
                 <p className="text-[13px] mt-1 text-gray-700">
                     Add Story
                 </p>
             </div>
-
+            {stories.map((story, index) => (
+                <div key={index} className="flex flex-col items-center shrink-0">
+                    <div className="w-[78px] h-[78px] rounded-full bg-gradient-to-tr from-yellow-400 via-pink-500 to-purple-600 p-[2.5px]" >
+                        <img src={story} alt="story" className="w-full h-full rounded-full object-cover border-2 border-white" />
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };
